162dae4a72c25dad6224a950719e5e5ba89b1045
melxx <melusimgwenya@gmail.com>
2021-06-02 01:57:29 +0200

    added code

--- lib/DashboardScreens/Market.dart
+++ lib/DashboardScreens/Market.dart
@@ -9,61 +9,138 @@ class Market extends StatefulWidget {
 }
 
 class _MarketState extends State<Market> {
-  var _selectedIndex =1;
+  var _selectedIndex = 1;
   void _onItemTapped(int index) {
     print(index);
     setState(() {
       _selectedIndex = index;
-      switch(index){
+      switch (index) {
         case 0:
-        // do something
+          // do something
           break;
         case 1:
-        // do something else
+          // do something else
           break;
         case 2:
-        // do something
+          // do something
           break;
         case 3:
-        // do something else
+          // do something else
           break;
         case 4:
-        // do something else
+          // do something else
           break;
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
+    String dropdownValue = 'XRPUSD';
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         title: Text(DefaultVars.AppTitle),
       ),
       backgroundColor: DefaultVars.AppBackground,
-      body: Container(
+      body: Column(
+        children: <Widget>[
+           Row(
+              children: <Widget>[
 
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("ALL", style: TextStyle(fontSize: 25)),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(DefaultVars.BtnColor),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("FAV", style: TextStyle(fontSize: 25)),
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(DefaultVars.BtnColor),
+                    foregroundColor:
+                    MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("MARKET", style: TextStyle(fontSize: 25)),
+                  style: ButtonStyle(
+                    backgroundColor:
+                    MaterialStateProperty.all<Color>(DefaultVars.BtnColor),
+                    foregroundColor:
+                    MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ElevatedButton(
+            style: ButtonStyle(
+              backgroundColor:
+              MaterialStateProperty.all<Color>(DefaultVars.BtnColor),
+              foregroundColor:
+              MaterialStateProperty.all<Color>(Colors.black),
+            ),
+            onPressed: (){},
+            child: DropdownButton<String>(
+              value: dropdownValue,
+              icon: const Icon(Icons.arrow_downward),
+              iconSize: 24,
+              elevation: 16,
+              style: const TextStyle(color: Colors.black),
+
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+              items: <String>['XRPUSD', 'DOGEUSD', 'ETHUSD', 'XLMUSD']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          Card(
+            child: Row(
+              
+            ),
+          ),
+          Card(),
+          Card(),
+          Card(),
+        ],
       ),
-      bottomNavigationBar: DefaultVars.getBottom(1,(int index) {
+      bottomNavigationBar: DefaultVars.getBottom(1, (int index) {
         print(index);
         setState(() {
           _selectedIndex = index;
 
-          switch(index){
+          switch (index) {
             case 0:
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => Dashboard()));
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Dashboard()));
               break;
             case 1:
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => Market()));
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => Market()));
               break;
             case 2:
-            // do something
+              // do something
               break;
             case 3:
-            // do something else
+              // do something else
               break;
             case 4:
-            // do something else
+              // do something else
               break;
           }
         });
