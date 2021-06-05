aeae59129182c37e54d5831472152aa91993536d
melxx <melusimgwenya@gmail.com>
2021-06-05 15:26:14 +0200

    added crypto symbols

--- lib/DashboardScreens/Market.dart
+++ lib/DashboardScreens/Market.dart
@@ -104,6 +104,111 @@ class _MarketState extends State<Market> {
       backgroundColor: DefaultVars.AppBackground,
       body: Column(
         children: <Widget>[
+          Card(
+              color: Colors.white,
+              elevation: 10.0,
+              //** use fl charts **
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: EdgeInsets.all(5),
+                child: ListTile(
+                  contentPadding:
+                  EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                  leading: Image(
+                    image: AssetImage('btc.png'),
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    height: MediaQuery.of(context).size.width * 0.45,
+                  ),
+                  title: Text("Bitcoin\nBTC"),
+                  trailing: Text("Current Price\n\$35000,60"),
+                  onTap: (){},
+                ),
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            elevation: 10.0,
+            //** use fl charts **
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(5),
+              child: ListTile(
+                contentPadding:
+                EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                leading: Image(
+                  image: AssetImage('ripple_xrp.png'),
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  height: MediaQuery.of(context).size.width * 0.45,
+                ),
+                title: Text("Ripple\nXRP"),
+                trailing: Text("Current Price\n\$0,90"),
+                onTap: (){},
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            elevation: 10.0,
+            //** use fl charts **
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(5),
+              child: ListTile(
+                contentPadding:
+                EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                leading: Image(
+                  image: AssetImage('doge.png'),
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  height: MediaQuery.of(context).size.width * 0.45,
+                ),
+                title: Text("Dogecoin\nDODGE"),
+                trailing: Text("Current Price\n\$0,60"),
+                onTap: (){},
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            elevation: 10.0,
+            //** use fl charts **
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(5),
+              child: ListTile(
+                contentPadding:
+                EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                leading: Image(
+                  image: AssetImage('btc.png'),
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  height: MediaQuery.of(context).size.width * 0.45,
+                ),
+                title: Text("Etherum\nETH"),
+                trailing: Text("Current Price\n\$1000"),
+                onTap: (){},
+              ),
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            elevation: 10.0,
+            //** use fl charts **
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(5),
+              child: ListTile(
+                contentPadding:
+                EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                leading: Image(
+                  image: AssetImage('ltc.png'),
+                  width: MediaQuery.of(context).size.width * 0.45,
+                  height: MediaQuery.of(context).size.width * 0.45,
+                ),
+                title: Text("Litecoin\nLTC"),
+                trailing: Text("Current Price\n\$1000,60"),
+                onTap: (){},
+              ),
+            ),
+          ),
         ],
       ),
       bottomNavigationBar: DefaultVars.getBottom(1, (int index) {
